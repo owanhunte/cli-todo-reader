30b5368a0d6a745c60ae53ac97784be429e0cbf0
Owan Hunte <owan@blockadjacent.com>
2024-04-15 05:16:46 -0400

    test: install + setup Jest and add initial tests

---
+++ test/helper.test.js
@@ -0,0 +1,42 @@
+import { Command } from "commander";
+import { jest } from "@jest/globals";
+import { getOptions } from "../src/helper";
+
+describe("helper getOptions tests", () => {
+  /** @type {Command} */
+  let program;
+
+  beforeAll(() => {
+    program = new Command();
+  });
+
+  it("should return all parsed options", () => {
+    const parsedOptions = {
+      count: 20,
+      selection: "even"
+    };
+
+    const spy = jest.spyOn(program, "opts").mockImplementationOnce(() => {
+      return parsedOptions;
+    });
+
+    const options = getOptions(program);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(options).toMatchObject(parsedOptions);
+  });
+
+  it("should convert options.count to number if parsed option is a string", () => {
+    const spy = jest.spyOn(program, "opts").mockImplementationOnce(() => {
+      return {
+        count: "10",
+        selection: "even"
+      };
+    });
+
+    const options = getOptions(program);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(options.count).toStrictEqual(10);
+  });
+});
