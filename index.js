66b9e1a16d4f35e25433c26b6840a3ce5e015202
Owan Hunte <owan@blockadjacent.com>
2024-04-14 22:48:52 -0400

    refactor: no need to store env vars in constants, just use process.env.* always

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ dotenv.config();
 import { Command } from "commander";
 import { getOptions } from "./src/helper.js";
 
-const DEFAULT_READER_COUNT = process.env.DEFAULT_READER_COUNT;
-const DEFAULT_SELECTION = process.env.DEFAULT_SELECTION;
-
 const program = new Command();
 
 program
@@ -17,11 +14,11 @@ program
     "Consume the first 'n' even or odd numbered TODOs from a pre-determined resource URL and output the title and whether it is completed or not."
   )
   .version("1.0.0")
-  .option("-c, --count <count>", "The number of TODOs to consume.", parseInt(DEFAULT_READER_COUNT))
+  .option("-c, --count <count>", "The number of TODOs to consume.", parseInt(process.env.DEFAULT_READER_COUNT))
   .option(
     "-s, --selection <even | odd>",
     "The selection of TODOs to consume, if even or odd numbered.",
-    DEFAULT_SELECTION
+    process.env.DEFAULT_SELECTION
   );
 
 program.parse();
